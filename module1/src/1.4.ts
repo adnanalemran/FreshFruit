dd9ba0cbf808183a86e46fd9dd0366a5c166c445
adnanalemran <adnanalemranontor@gmail.com>
2024-10-08 00:00:09 +0600

    Refactor code to add basic data types and arrays
    This commit refactors the code in module1/src/1.4.ts to add basic data types such as string, number, boolean, undefined, and null. It also introduces arrays for both strings and numbers. Additionally, a tuple type is defined in the code. The changes aim to improve the clarity and organization of the code.

--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -1,3 +1,31 @@
 //basic data type
 
 //string
+
+let firstName: string = "adnan";
+//number
+let roll: number = 123;
+//boolean
+let isAdmin: boolean = true;
+//undefined
+let x: undefined = undefined;
+//null
+let y: null = null;
+
+let d: number;
+d = 123;
+
+//none primitive data type
+//array string
+let friends: string[] = ["ali", "ahmed", "umer"];
+
+//array number
+let marks: number[] = [12, 23, 34];
+
+friends.push("ontor");
+
+//tuple type --> array with fixed number of elements
+let info: object = {
+  name: "adnan",
+  age: 23,
+};
